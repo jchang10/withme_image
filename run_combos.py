ff313da850ddf03bbfd020e131b66f4be1612a67
Jae Chang <jc-github@jline.com>
2017-09-26 13:20:35 -0700

    good working version
    ---------- Forwarded message ----------
    From: Jae Chang <jchang@imvu.com>
    Date: Tue, Sep 26, 2017 at 12:43 PM
    Subject: Latest Images Update
    To: Robert Otani <rotani@imvu.com>
    
    Quick update. I attached a contact sheet with icons of a random sampling. I think this is the best they have looked, yet.
    
    The latest index now contains 64M rows. That is down from the original 700M. Here are some more random specs.
    
    image size: 300x325
    average file size: 60 kb
    create time: 10 images per second
    time to process entire index:
      size: 3,862 gb
      rows 64,365,839 / 10 / 3600 = 1788 hours = 74 days
    time to process 1M images:
      size: 60 gb
      rows 1M / 10 / 3600 = 28 hours
    
    Current list of filters:
    - head: skip if the skin does not match
    - nose and mouths: match light skin noses
                                    match dark skin noses
    - hairs: hair9 to 19 all marked as gender:female. the rest are either gender.
        do not match any facial hair for female hairs
    
    That's it so far.
    
    Jae

--- run_combos.py
+++ run_combos.py
@@ -1,83 +1,111 @@
 import os, sys
-
+assert sys.version.startswith('3.6'), "Python version 3.6 is required"
 
 items_path='items'
 
 db = {
-    'head'      :{'files':[],       'done':False},
-    'eye'       :{'files':[],       'done':False},
-    'eyebrow'   :{'files':[],       'done':False},
-    'mouth'     :{'files':[],       'done':False},
-    'facehair'  :{'files':['none'], 'done':False},
-    'nose'      :{'files':[],       'done':False},
-    'hair'      :{'files':['none'], 'done':False},
-    'spectacle' :{'files':['none'], 'done':False},
-    }
+    'skin'      :{'files':{},       'done':False},
+    'head'      :{'files':{},       'done':False},
+    'eye'       :{'files':{},       'done':False},
+    'eyebrow'   :{'files':{},       'done':False},
+    'mouth'     :{'files':{},       'done':False},
+    'facehair'  :{'files':{'none':{}}, 'done':False},
+    'nose'      :{'files':{},       'done':False},
+    'hair'      :{
+        'files':{
+            'none':{},
+            'hair9':{'gender':'f'},
+            'hair10':{'gender':'f'},
+            'hair11':{'gender':'f'},
+            #'hair12':{'gender':'f'},
+            'hair13':{'gender':'f'},
+            'hair14':{'gender':'f'},
+            'hair15':{'gender':'f'},
+            'hair16':{'gender':'f'},
+            'hair17':{'gender':'f'},
+            'hair19':{'gender':'f'},
+    }, 'done':False },
+    'spectacle' :{'files':{'none':{}}, 'done':False},
+}
 
-items=list(db.keys())
+item_keys=list(db.keys())
 
 def create_db():
-    for k in items:
+    for k in item_keys:
         path = os.path.join(items_path, k+'s')
-        for file in os.listdir(path):
-            if file[-4:] == '.png':
-                file = file[:-4]
-                db[k]['files'].append(file)
+        for filename in os.listdir(path):
+            if filename[-4:] == '.png':
+                name = filename[:-4]
+                files = db[k]['files']
+                if name in files:
+                    pass
+                else:
+                    files[name] = {}
     return db
     
-def item_generator(path, index):
-    """ Run through all combinations """
-    if index == len(items):
-        yield path
-        return
-    for filename in db[items[index]]['files']:
-        newpath = str.join('-', (path, filename))
-        yield from item_generator(newpath, index+1)
-
-def flat_generator(path, index):
-    """ Just run through all the items """
-    if index == len(items):
-        yield path
-        return
-    if db[items[index]]['done']:
-        filename = db[items[index]]['files'][0]
-        newpath = str.join('-', (path, filename))
-        yield from flat_generator(newpath, index+1)
-        return
-    for filename in db[items[index]]['files']:
-        newpath = str.join('-', (path, filename))
-        yield from flat_generator(newpath, index+1)
-    db[items[index]]['done'] = True
-
 count = 0            
 def item_recurse(path, index):
     """ Same as item_generator() but done recurisvely """
     global count
-    if index == len(items):
+    if index == len(item_keys):
         print(count,path,sep='')
         count +=1
         return
-    for filename in db[items[index]]['files']:
+    for filename in db[item_keys[index]]['files']:
         newpath = str.join('-', (path, filename))
         item_recurse(newpath, index+1)
             
-def run():
-    for i, item in enumerate(item_generator('', 0)):
-        print(i, item, sep='')
+def item_generator(path, index):
+    """ Run through all combinations """
+    if index == len(item_keys):
+        yield path
+        return
+    key = item_keys[index]
+    item = db[item_keys[index]]
+    files = item['files']
+    for filename in files:
+        if key == 'head':
+            skin = path[0][-1]
+            #skip if the skin does not match
+            if filename[-2] != skin:
+                continue
+        if key == 'nose':
+            skin = path[0][-1]
+            # match light skin noses
+            if skin in ('a','b') and filename[-2] != 'e':
+                continue
+            # match dark skin noses
+            if skin in ('c','d') and filename[-2] != 'b':
+                continue
+        if key == 'mouth':
+            skin = path[0][-1]
+            if skin in ('a','b') and filename[-2] != 'h':
+                continue
+            if skin in ('c','d') and filename[-2] != 'b':
+                continue
+        if key == 'hair':
+            facehair = path[5]
+            # if hair is female and has facehair, skip 
+            if 'gender' in files[filename] and facehair != 'none':
+                continue
+            # skip if the filename ends in 'b'. only used for back hair
+            if filename[-1] == 'b':
+                continue
+        newpath = path + [filename]
+        yield from item_generator(newpath, index+1)
+
+def run_items():
+    for i, filenames in enumerate(item_generator([], 0)):
+        print(i, '-', '-'.join(filenames), sep='')
 
 def run_recurse():
     item_recurse('', 0)
-
-def run_flat():
-    for i, item in enumerate(flat_generator('', 0)):
-        print(i, item, sep='')
 
 if __name__ == '__main__':
     assert len(sys.argv) > 1, "Items path argument is missing."
     items_path=sys.argv[1] 
     assert os.path.exists(items_path), "Given items path does not exist."
     create_db()
-    #run()
+    run_items()
     #run_recurse()
-    run_flat()
     
--- run_image1.py
+++ run_image1.py
@@ -1,5 +1,5 @@
-
 import os, sys
+assert sys.version.startswith('3.6'), "Python version 3.6 is required"
 
 from PIL import Image
 
@@ -12,15 +12,17 @@ items_path='items'
 output_path='output_path'
 
 db = {
-    'head'      :{'file':{'name':None}, 'offsets':(72, 238)},
-    'eye'       :{'file':{'name':None}, 'offsets':(72, 448)},
-    'eyebrow'   :{'file':{'name':None}, 'offsets':(72, 411)},
-    'mouth'     :{'file':{'name':None}, 'offsets':(212, 966)},
-    'facehair'  :{'file':{'name':None}, 'offsets':(212, 816)},
-    'nose'      :{'file':{'name':None}, 'offsets':(421, 667)},
-    'hair'      :{'file':{'name':None}, 'offsets':(-29, 38)},
-    'spectacle' :{'file':{'name':None}, 'offsets':(71, 467)},
-    }
+    'skin'      :{'file':{'name':'to-image'}, 'offset':(-25, 500)},
+    'head'      :{'file':{'name':'to-image'}, 'offset':(72, 238)},
+    'eye'       :{'file':{'name':'to-image'}, 'offset':(72, 499)},
+    'eyebrow'   :{'file':{'name':'to-image'}, 'offset':(72, 470)},
+    'mouth'     :{'file':{'name':'to-image'}, 'offset':(72, 888)},
+    'facehair'  :{'file':{'name':'to-image'}, 'offset':(72, 700)},
+    'nose'      :{'file':{'name':'to-image'}, 'offset':(421, 657)},
+    'hair'      :{'file':{'name':'to-image'}, 'offset':(-29, 38),
+                  'back':{'file':{'name':'to-image'}}},
+    'spectacle' :{'file':{'name':'to-image'}, 'offset':(71, 509)},
+}
 item_keys=list(db.keys())
 
 def create_db():
@@ -30,22 +32,60 @@ def create_db():
                 name = file[:-4]
                 path = os.path.join(items_path, k+'s', file)
                 image = Image.open(path)
-                db[k]['file'][name] = image
+                if k == 'hair' and file[-5:] == 'b.png':
+                    #back of the hair file
+                    db[k]['back'][name] = image
+                else:
+                    db[k]['file'][name] = image
     return db
 
 def process_line(line):
     index, *items = line.split('-')
     print(index, end=' ', flush=True)
     image = Image.new('RGB', (WIDTH, HEIGHT), color='white')
+    trans = Image.new('RGBA', (WIDTH, HEIGHT))
     for i, name in enumerate(items):
         k = item_keys[i]
         if name in db[k]['file']:
             im = db[k]['file'][name]
-            image.paste(im, db[k]['offsets'], im)
+            trans.paste(im, db[k]['offset'], im)
+        back_name = name+'b'
+        if back_name in db[k].get('back', []):
+            im = db[k]['back'][back_name]
+            image.paste(im, db[k]['offset'], im)
+    image.paste(trans, (0,0), trans)
     path = os.path.join(output_path,line+'.png')
-    if DEBUG: print('saving image '+path)
-    if not DEBUG: image.save(path)
+    if DEBUG:
+        print('saving image '+path)
+    else:
+        im2 = image.resize((int(image.width/4),int(image.height/4)), Image.ANTIALIAS)
+        im2.save(path)
 
+def process_infile(infile_path):
+    with open(infile_path) as infile:
+        for line in infile:
+            process_line(line.rstrip())
+    print()
+
+outputs = []
+def process_random(infile_path):
+    print("Reading infile")
+    line = None
+    index = -1
+    with open(infile_path) as infile:
+        for line in infile:
+            index, _ = line.split('-', maxsplit=1)
+            outputs.append(line.rstrip())
+    print("Done with infile. Index =",index)
+    print("Processing images ...")
+    from random import randint
+    size = len(outputs)
+    while size:
+        size = size-1
+        ri = randint(0, size)
+        process_line(outputs[ri])
+        outputs[ri] = outputs[size]
+    
 if __name__ == '__main__':
     assert len(sys.argv) == 4, "3 args missing. <infile> <items> <output>"
     _, infile_path, items_path, output_path = sys.argv
@@ -53,7 +93,5 @@ if __name__ == '__main__':
     assert os.path.isdir(items_path), "Given <items> must be a dir."
     assert os.path.isdir(output_path), "Given <output> must be a dir."
     create_db()
-    with open(infile_path) as infile:
-        for line in infile:
-            process_line(line.rstrip())
-    print()
+    process_random(infile_path)
+    
